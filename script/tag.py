ff9b366483bdbb4e5b0dd100afc13b467ef9dcc5
Lotuses-robot <robot7833@vip.qq.com>
2023-03-03 20:17:22 +0800

    VER 0.1.2 now it's ok?

--- script/tag.py
+++ script/tag.py
@@ -2,9 +2,9 @@ import json
 import os
 import re
 
-rb = re.split("^[^\n]+\n\n", json.loads(os.environ["MSG"])[-1])
-b = rb[1] if len(rb) > 1 else rb[0]
-open("./src/update-log.txt", "w").write(b)
+# rb = re.split("^[^\n]+\n\n", json.loads(os.environ["MSG"])[-1])
+# b = rb[1] if len(rb) > 1 else rb[0]
+# open("./src/update-log.txt", "w").write(b)
 print(json.loads(open('./package.json').read())['version'])
 
 # thank for exlg's code
