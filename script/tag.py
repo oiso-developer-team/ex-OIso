f9dcbee49a7c9b6accd45ad765da08baa41922d2
Lotuses-robot <robot7833@vip.qq.com>
2023-03-03 20:13:21 +0800

    VER 0.1.0 what fking wrong

--- script/tag.py
+++ script/tag.py
@@ -4,7 +4,7 @@ import re
 
 rb = re.split("^[^\n]+\n\n", json.loads(os.environ["MSG"])[-1])
 b = rb[1] if len(rb) > 1 else rb[0]
-open("./src/resources/update-log.txt", "w").write(b)
+open("./src/update-log.txt", "w").write(b)
 print(json.loads(open('./package.json').read())['version'])
 
 # thank for exlg's code
